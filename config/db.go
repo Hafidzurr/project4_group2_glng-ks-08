b8cd980247bd983eda38f9c50d5794c64fbe6011
Hafidzurr <hafidzurrohmansaifullah@gmail.com>
2023-11-22 21:50:34 +0700

    railway prepare

--- config/db.go
+++ config/db.go
@@ -2,16 +2,34 @@ package config
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/Hafidzurr/project4_group2_glng-ks-08/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"os"
+)
+
+var (
+	host     = os.Getenv("PGHOST")
+	user     = os.Getenv("PGUSER")
+	password = os.Getenv("PGPASSWORD")
+	dbPort   = os.Getenv("PGPORT")
+	dbname   = os.Getenv("PGDATABASE")
+	// host       = "localhost"
+	// user       = "postgres"
+	// password   = "postgres890"
+	// dbPort     = "5432"
+	// dbname     = "mygram"
+	// debug_mode = os.Getenv("DEBUG_MODE")
+	DB  *gorm.DB
+	err error
 )
 
 func ConnectDB() (*gorm.DB, error) {
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
-		dsn = "host=localhost user=postgres dbname=market password=Hafidzurr1 sslmode=disable" // Lokal DB config
+		// dsn = "host=host user=postgres dbname=market password=Hafidzurr1 sslmode=disable" // Lokal DB config
+		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, dbPort)
 	}
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
